c894623ca36dffdeeff76e5012e157ee10f9a448
Asim Saeed <asimsaeed353@gmail.com>
2024-12-24 10:55:20 +0500

    Completed Lesson 24

---
+++ 24 - Programming is Rewriting/views/notes.view.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en" class="h-full bg-gray-100">
+<head>
+    <meta charset="UTF-8">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>Page Links</title>
+</head>
+<body class="h-full">
+
+<div class="min-h-full">
+    <?php require ('partials/nav.php') ?>
+
+    <?php require ('partials/header.php') ?>
+
+    <main>
+        <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+
+<!--            display notes as list items-->
+
+            <?php foreach ($notes as $note) : ?>
+                <li>
+                    <a href="/note?id=<?= $note['id']?>" class="text-blue-500 hover:underline">
+                    <?= $note['body'] ?>
+                    </a>
+                </li>
+            <?php endforeach; ?>
+        </div>
+    </main>
+
+</div>
+
+</body>
+</html>

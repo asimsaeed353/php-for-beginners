a4d6b5a018b910962873de91921594210415836f
Asim Saeed <asimsaeed353@gmail.com>
2025-01-02 15:00:44 +0500

    Completed 38 Lesson

---
+++ 38 - Register a New User/Core/functions.php
@@ -0,0 +1,47 @@
+<?php
+
+
+// the functions in this file will be used in all php controller files.
+
+use Core\Response;
+
+function dd($superGlobal)
+{
+echo "<pre>";
+    var_dump($superGlobal);
+    echo "</pre>";
+
+die(); // stop the execution
+}
+
+function urlIs($variable){
+    return ($_SERVER['REQUEST_URI'] == $variable);
+}
+
+function abort($code = 404)
+{
+    http_response_code($code);
+
+    require base_path("views/partials/{$code}.php");
+
+    die();
+}
+
+// Check a condition and abort if it is false
+function authorize($condition, $status = Response::FORBIDDEN){
+    if(! $condition){
+        abort($status);
+    }
+}
+
+// create a base_path generator
+function base_path($path){
+    return BASE_PATH . $path;
+}
+
+// create a method to load views
+function view($path, $attributes = [])
+{
+    extract($attributes);
+    require base_path('/views/' . $path);  // output base_path/views/file_name.view.php
+}
---
+++ 38 - Register a New User/routes.php
@@ -0,0 +1,27 @@
+<?php
+
+
+$router->get('/', '/controllers/index.php');
+$router->get('/about', 'controllers/about.php');
+$router->get('/contact', 'controllers/contact.php');
+
+$router->get('/notes', 'controllers/notes/index.php');
+$router->get('/note', 'controllers/notes/show.php');
+$router->delete('/note', '/controllers/notes/destroy.php');
+
+// Edit and Update Note
+$router->get('/note/edit', 'controllers/notes/edit.php');
+$router->patch('/note', 'controllers/notes/update.php');
+
+// Show notes
+$router->get('/notes/create', 'controllers/notes/create.php');
+// create a note
+$router->post('/notes', 'controllers/notes/store.php');
+
+// registering a new user
+
+$router->get('/register', '/controllers/registration/create.php');
+$router->post('/register', '/controllers/registration/store.php');
+
+
+

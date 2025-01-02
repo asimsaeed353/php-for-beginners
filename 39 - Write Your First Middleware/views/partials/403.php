3ac145928ec935a19154ca2b2f922be7022bf14a
Asim Saeed <asimsaeed353@gmail.com>
2025-01-02 16:25:58 +0500

    Completed 39 lesson
    This lesson was about middleware for authentication, authorization.

---
+++ 39 - Write Your First Middleware/views/partials/403.php
@@ -0,0 +1,27 @@
+404.php<!DOCTYPE html>
+<html lang="en" class="h-full bg-gray-100">
+<head>
+    <meta charset="UTF-8">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>Page Links</title>
+</head>
+<body class="h-full">
+
+<div class="min-h-full">
+    <?php require base_path('views/partials/nav.php') ?>
+
+    <!--    --><?php //require ('views/partials/header.php') ?>
+
+    <main>
+        <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+            <h3>403 Error</h3>
+            <p>Opps, You are not authorised to access this page.</p>
+
+            <a href="/" class="text-blue underline">Go back to home page</a>
+        </div>
+    </main>
+
+</div>
+
+</body>
+</html>
